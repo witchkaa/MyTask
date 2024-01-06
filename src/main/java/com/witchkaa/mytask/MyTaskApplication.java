03fd155e7c6c469b2565ef6625bceedd9213a286
witchkaa <victoriasabadan1@gmail.com>
2024-01-06 11:39:32 +0200

    connect to postgres

--- src/main/java/com/witchkaa/mytask/MyTaskApplication.java
+++ src/main/java/com/witchkaa/mytask/MyTaskApplication.java
@@ -1,26 +1,15 @@
 package com.witchkaa.mytask;
 
-import com.witchkaa.mytask.entity.Task;
-import com.witchkaa.mytask.service.TaskService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+
+
 
 @SpringBootApplication
 public class MyTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyTaskApplication.class, args);
-    }
-    @Bean
-    CommandLineRunner runner(TaskService service) {
-        return args -> {
-            service.saveTask(new Task(1L, "Walk the dog", LocalDate.now().plus(2, ChronoUnit.DAYS), false));
-            service.saveTask(new Task(2L, "Do hometask", LocalDate.now(), false));
-        };
     }
 }
--- src/main/java/com/witchkaa/mytask/entity/Task.java
+++ src/main/java/com/witchkaa/mytask/entity/Task.java
@@ -1,9 +1,8 @@
 package com.witchkaa.mytask.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -14,12 +13,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name="tasks")
 public class Task {
     @Id
-    @GeneratedValue
+    @JsonProperty("id")
     private Long id;
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("deadline")
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate deadline;
+    @JsonProperty("is_done")
     private Boolean isDone;
 }
